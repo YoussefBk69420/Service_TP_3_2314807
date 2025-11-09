f69e701f37e5c2d48f30e78a464428d1f493b94e
YoussefBk69420 <2314807@cegepmontpetit.ca>
2025-11-08 21:04:49 -0500

    FCT Modification de score.component
    Modification de score.component pour utiliser le score.service

--- flappyBirb_client/src/app/score/score.component.ts
+++ flappyBirb_client/src/app/score/score.component.ts
@@ -3,6 +3,7 @@ import { Score } from '../models/score';
 import { MaterialModule } from '../material.module';
 import { CommonModule } from '@angular/common';
 import { Round00Pipe } from '../pipes/round-00.pipe';
+import { ScoresService } from '../services/scores.service';
 
 @Component({
   selector: 'app-score',
@@ -16,17 +17,19 @@ export class ScoreComponent {
   myScores : Score[] = [];
   publicScores : Score[] = [];
 
-  constructor() { }
+  constructor(public scoresService : ScoresService) { }
 
   async ngOnInit() {
 
+    this.publicScores = await this.scoresService.getPublicScores();
+    this.myScores = await this.scoresService.getPrivateScores();
 
 
   }
 
   async changeScoreVisibility(score : Score){
 
-
+    await this.scoresService.changeScoreVisibility(score.id);
   }
 
 }
